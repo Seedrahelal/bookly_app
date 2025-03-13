176c7ad30e3aa5ef707faae6948c881ce5d050c2
Seedrahelal <sedeahelal16@gmail.com>
2025-03-13 21:10:44 +0300

    best seller list view done

--- lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
@@ -24,36 +25,45 @@ class BestSellerListViewItem extends StatelessWidget {
               ),
             ),
             const SizedBox(width: 30),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .5,
-                  child: Text(
-                    'Harry Potter and the Goblet of Fire',
-                    style: Styles.textStyle22.copyWith(
-                      fontFamily: kFontFamily,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .5,
+                    child: Text(
+                      'Harry Potter and the Goblet of Fire',
+                      style: Styles.textStyle20.copyWith(
+                        fontFamily: kFontFamily,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
                   ),
-                ),
-                const SizedBox(height: 3),
-                const Text(
-                  'J.K. Rowling',
-                  style: Styles.textStyle16,
-                ),
-                const SizedBox(height: 3),
-                Row(
-                  children: [
-                    Text(
-                      r'19.19 $',
-                      style: Styles.textStyle22
-                          .copyWith(fontWeight: FontWeight.bold),
-                    )
-                  ],
-                )
-              ],
+                  const SizedBox(height: 5),
+                  const Text(
+                    'J.K. Rowling',
+                    style: Styles.textStyle16,
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    children: [
+                      Text(r'19.19 $',
+                          style: Styles.textStyle18
+                              .copyWith(fontWeight: FontWeight.bold)),
+                      const Spacer(),
+                      const Icon(FontAwesomeIcons.solidStar,
+                          color: Color(0xffFFDD4F)),
+                      const SizedBox(width: 6.3),
+                      const Text('4.8', style: Styles.textStyle16),
+                      const SizedBox(width: 5),
+                      Text('(245)',
+                          style: Styles.textStyle14
+                              .copyWith(color: const Color(0xff707070))),
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
