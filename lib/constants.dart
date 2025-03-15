61c022762b0f120ef3bf527bbf32d1aed8f4e264
Seedrahelal <sedeahelal16@gmail.com>
2025-03-15 21:49:47 +0300

    create search view

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,4 +4,6 @@ const kPrimaryColor = Color(0xff100B20);
 const kTransitionDuration = Duration(seconds: 2);
 const kFontFamily = 'Poppins';
 const kHomeView = '/homeView';
+const kSearchView = '/searchView';
+
 const kBookDetailsView = '/bookDetailsView';
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/Features/Splash/presentation/views/splash_view.dart';
 import 'package:bookly_app/Features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/Features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/Features/search/presentation/views/search_view.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:go_router/go_router.dart';
 
@@ -18,6 +19,10 @@ abstract class AppRouter {
       GoRoute(
         path: kHomeView,
         builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (context, state) => const SearchView(),
       )
     ],
   );
