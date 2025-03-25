0c35511a858886f090cfa6f76b450d23b4d979f6
Seedrahelal <sedeahelal16@gmail.com>
2025-03-26 01:25:14 +0300

    implement  fetch newset books

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:bookly_app/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failures, List<BookModel>>> fetchNewsetBooks();
   Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks();
 }
