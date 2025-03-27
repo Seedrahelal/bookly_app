6f612b81a36b8155bbb94d6c78aee1bad881de63
Seedrahelal <sedeahelal16@gmail.com>
2025-03-27 21:43:56 +0300

    setup service locator

--- lib/Features/home/presentation/manager/newset_books/newset_books_cubit.dart
+++ lib/Features/home/presentation/manager/newset_books/newset_books_cubit.dart
@@ -7,16 +7,13 @@ part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
   NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
-  
+
   final HomeRepo homeRepo;
 
   Future<void> fetchNewestBooks() async {
     emit(NewsetBooksLoading());
     var result = await homeRepo.fetchNewestBooks();
-    result.fold((failure) => {
-      emit(NewsetBooksFailure(failure.errMessage))
-    }, (books) => {
-      emit(NewsetBooksSuccess(books))
-    });
+    result.fold((failure) => {emit(NewsetBooksFailure(failure.errMessage))},
+        (books) => {emit(NewsetBooksSuccess(books))});
   }
 }
